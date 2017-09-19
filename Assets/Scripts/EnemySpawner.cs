18136af94594541e50a1b91334b3b0caf921abe5
James Fonzone <jfonzone@outlook.com>
2017-09-19 18:07:33 -0400

    spawn enemies at positions

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,9 +8,9 @@ public class EnemySpawner : MonoBehaviour {
 	void Start () {
 		//for every Transform component in this transform
 		foreach(Transform child in transform){
-			GameObject enemy = Instantiate(enemyPrefab, new Vector3(0,0,0), Quaternion.identity) as GameObject;
+			GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
 			//instantiate enemy as child of this child transform (Position object)
-			enemy.transform.parent = transform;
+			enemy.transform.parent = child;
 		}
 	}
 	
