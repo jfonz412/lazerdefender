653d088235be3703d22694ecb2a0599b11b1bf63
James Fonzone <jfonzone@outlook.com>
2017-09-19 17:58:16 -0400

    testing spawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,9 +6,12 @@ public class EnemySpawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GameObject enemy = Instantiate(enemyPrefab, new Vector3(0,0,0), Quaternion.identity) as GameObject;
-		//instantiate enemy as child of this enemy spawner
-		enemy.transform.parent = transform;
+		//for every Transform component in this transform
+		foreach(Transform child in transform){
+			GameObject enemy = Instantiate(enemyPrefab, new Vector3(0,0,0), Quaternion.identity) as GameObject;
+			//instantiate enemy as child of this child transform (Position object)
+			enemy.transform.parent = transform;
+		}
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/Position.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Position : MonoBehaviour {
+	//draw sphere around position object in editor
+	void OnDrawGizmos(){
+		Gizmos.DrawWireSphere(transform.position, 1);
+	}
+}
