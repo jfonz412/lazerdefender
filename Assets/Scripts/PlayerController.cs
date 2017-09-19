c01e78b904717ec6eba02878d41c2f4df48a4441
James Fonzone <jfonzone@outlook.com>
2017-09-19 15:28:21 -0400

    player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 10.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		MoveShip();
+	}
+	
+	//transform does not need to be defined apprently
+	void MoveShip(){
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0); //speed independent from framerate
+		} 
+		else if(Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+		}
+	}
+}
