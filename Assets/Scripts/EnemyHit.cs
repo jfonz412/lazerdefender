12109f851f2927a707d01af9c5e3e03d55246f5c
James Fonzone <jfonzone@outlook.com>
2017-09-20 18:46:13 -0400

    shoot and destroy enemy

---
+++ Assets/Scripts/EnemyHit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHit : MonoBehaviour {
+	public float health = 150;
+
+	void OnTriggerEnter2D(Collider2D col){
+		// check if col has Projectile script/component
+		Projectile lazer = col.gameObject.GetComponent<Projectile>();
+		if (lazer){
+			health -= lazer.GetDamage();
+			if (health <= 0){
+				Destroy(gameObject);
+			}
+			lazer.Hit ();
+			Debug.Log ("Hit by projectile");
+		}
+	}
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	public float damage = 100f;
+	
+	//will be called by enemy on collision to recieve damage
+	public float GetDamage(){
+		return damage;
+	}
+	
+	//will be called by enemy on collision to destroy lazer
+	public void Hit(){
+		Destroy(gameObject);
+	}
+}
