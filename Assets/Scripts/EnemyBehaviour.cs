5e06964a2b806c3288731bfa6c2a3c774f49a9b0
James Fonzone <jfonzone@outlook.com>
2017-09-24 23:31:49 -0400

    add score UI

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -6,6 +6,13 @@ public class EnemyBehaviour : MonoBehaviour {
 	public float lazerSpeed; 
 	public float health = 150;
 	public float shotsPerSecond = 0.5f;
+	public int scoreValue = 150;
+	
+	private ScoreKeeper scoreKeeper;
+	
+	void Start(){
+		scoreKeeper = GameObject.Find("Score").GetComponent<ScoreKeeper>();
+	}
 
 	void OnTriggerEnter2D(Collider2D col){
 		// check if col has Projectile script/component
@@ -13,6 +20,7 @@ public class EnemyBehaviour : MonoBehaviour {
 		if (lazer){
 			health -= lazer.GetDamage();
 			if (health <= 0){
+				scoreKeeper.Score(scoreValue);
 				Destroy(gameObject);
 			}
 			lazer.Hit ();
---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour {
+	private int currentScore = 0;
+	private Text myText;
+	
+	void Start(){
+		myText = GetComponent<Text>();
+		myText.text = currentScore.ToString();
+	}
+	
+	public void Score(int points){
+		currentScore = currentScore + points;
+		myText.text = currentScore.ToString();
+	}
+	
+	void Reset(){
+		currentScore = 0;
+	}
+}
